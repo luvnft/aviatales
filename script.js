2aad82e95a5241a6bc7d3ae258aaa6428dfaa432
Tatiana <snow_white_girl@bk.ru>
2021-02-08 16:43:40 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Form Inputs
+const formSearch = document.querySelector('.form-search'),
+  inputCitiesFrom = document.querySelector('.input__cities-from'),
+  dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
+  inputCitiesTo = document.querySelector('.input__cities-to'),
+  inputDateDepart = document.querySelector('.input__date-depart');
+
+// Dropdowns
+const city = [
+  'Moscow',
+  'Saint-Petersburg',
+  'Samara',
+  'Minsk',
+  'Yekaterinburg',
+  'Novosibirsk',
+  'Kaliningrad',
+  'Sochi',
+  'Rostov-on-Don',
+];
+
+// Input events
+inputCitiesFrom.addEventListener('input', () => {});
