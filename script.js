d5bb3b5ffbd41f70dcfd5534b2886d7f8ab82057
Tatiana <snow_white_girl@bk.ru>
2021-02-09 09:27:01 +0300

    Input event added

--- script.js
+++ script.js
@@ -16,9 +16,26 @@ const city = [
   'Yekaterinburg',
   'Novosibirsk',
   'Kaliningrad',
+  'Kazan',
   'Sochi',
   'Rostov-on-Don',
 ];
 
 // Input events
-inputCitiesFrom.addEventListener('input', () => {});
+inputCitiesFrom.addEventListener('input', () => {
+  dropdownCitiesFrom.textContent = '';
+
+  if (inputCitiesFrom.value !== '') {
+    const filterCity = city.filter(item => {
+      const fixItem = item.toLowerCase();
+      return fixItem.includes(inputCitiesFrom.value.toLowerCase());
+    });
+    filterCity.forEach(item => {
+      const li = document.createElement('li');
+      li.classList.add('dropdown__city');
+      li.textContent = item;
+      dropdownCitiesFrom.append(li);
+      console.log(li);
+    });
+  }
+});
