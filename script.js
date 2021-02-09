ea33c0b7ea2ad07b7c08aa0233e3a79755f63074
Tatiana <snow_white_girl@bk.ru>
2021-02-09 11:45:01 +0300

    Added events for cities inputs and choosing city from dropdown

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const formSearch = document.querySelector('.form-search'),
   inputCitiesFrom = document.querySelector('.input__cities-from'),
   dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
   inputCitiesTo = document.querySelector('.input__cities-to'),
+  dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
   inputDateDepart = document.querySelector('.input__date-depart');
 
 // Dropdowns
@@ -21,21 +22,45 @@ const city = [
   'Rostov-on-Don',
 ];
 
-// Input events
-inputCitiesFrom.addEventListener('input', () => {
-  dropdownCitiesFrom.textContent = '';
+// Functions
+const showCity = (input, list) => {
+  list.textContent = '';
 
-  if (inputCitiesFrom.value !== '') {
+  if (input.value !== '') {
     const filterCity = city.filter(item => {
       const fixItem = item.toLowerCase();
-      return fixItem.includes(inputCitiesFrom.value.toLowerCase());
+      return fixItem.includes(input.value.toLowerCase());
     });
     filterCity.forEach(item => {
       const li = document.createElement('li');
       li.classList.add('dropdown__city');
       li.textContent = item;
-      dropdownCitiesFrom.append(li);
-      console.log(li);
+      list.append(li);
     });
+  }
+};
+
+// Events
+inputCitiesFrom.addEventListener('input', () => {
+  showCity(inputCitiesFrom, dropdownCitiesFrom);
+});
+
+dropdownCitiesFrom.addEventListener('click', event => {
+  const target = event.target;
+  if (target.tagName.toLowerCase() === 'li') {
+    inputCitiesFrom.value = target.textContent;
+    dropdownCitiesFrom.textContent = '';
+  }
+});
+
+inputCitiesTo.addEventListener('input', () => {
+  showCity(inputCitiesTo, dropdownCitiesTo);
+});
+
+dropdownCitiesTo.addEventListener('click', event => {
+  const target = event.target;
+  if (target.tagName.toLowerCase() === 'li') {
+    inputCitiesTo.value = target.textContent;
+    dropdownCitiesTo.textContent = '';
   }
 });
