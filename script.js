a935cebd3fdb52ea7e7e909acb9618726e8511b0
Tatiana <snow_white_girl@bk.ru>
2021-02-11 16:15:52 +0300

    Added date into cheapest ticket

--- script.js
+++ script.js
@@ -68,6 +68,16 @@ const getNameCity = code => {
   return objCity.name;
 };
 
+const getDate = date => {
+  return new Date(date).toLocaleString('ru', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+};
+
 const getChanges = num => {
   if (num) {
     return num === 1 ? 'С одной пересадкой' : 'С двумя пересадками';
@@ -95,7 +105,7 @@ const createCard = data => {
           <div class="city__from">Вылет из города
             <span class="city__name">${getNameCity(data.origin)}</span>
           </div>
-          <div class="date">${data.depart_date}</div>
+          <div class="date">${getDate(data.depart_date)}</div>
         </div>
 
         <div class="block-right">
